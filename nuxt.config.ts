aa4dd7a47a7eccc1ca350def0f0c4b92193c950d
Rafał Wesołowski <rafal.wesolowski@cec.valantic.com>
2023-08-28 15:21:34 +0200

    clean

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: [
-    '@/assets/css/main.css',
-    "@/assets/scss/main.scss"
+    '@/assets/css/main.css'
   ],
   postcss: {
     plugins: {
